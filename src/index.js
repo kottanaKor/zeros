96a2412009c06c031cc72c810032029c83a10bce
Tatsiana Korshunava <tachichoco@yahoo.com>
2018-09-30 23:41:08 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let zeros = 0;
+  let sum = number;
+  let divider = 5;
+  while (sum >= 5) {
+    sum = Math.floor(number / divider);
+    divider *= 5;
+    zeros += sum;
+  }
+  return zeros;
 }
